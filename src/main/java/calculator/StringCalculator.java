93e157f94cca7d3c76af9c618563efc3a6f13b59
Anamika Jaiswal <akimana333@gmail.com>
2021-10-18 01:40:24 +0530

    String with two numbers delimited by comma

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,7 +5,10 @@ class StringCalculator {
     public int add(String input) {
         if(input.isEmpty())
         	return 0;
-        else
+        else if(input.contains(",")) {
+        	String[] tokens = input.split(",");
+        	return Integer.parseInt(tokens[0]) + Integer.parseInt(tokens[1]);
+        } else 
         	return Integer.parseInt(input);
     }
 
